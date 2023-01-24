2b53ff6ed1ab310a1a4164f6f57440978684eba7
Nick Kraakman <nickkraakman@gmail.com>
2023-01-24 15:33:04 +0100

    Fix bugs and add serial logs

--- Arduino/batswap/batswap.ino
+++ Arduino/batswap/batswap.ino
@@ -1,6 +1,4 @@
-#include "config.h"
-
-const int SWAP_INTERVAL_SECONDS = 60;  // Seconds between battery swaps
+const int SWAP_INTERVAL_SECONDS = 10;  // Seconds between battery swaps
 const int SWAP_DURATION_MS = 40;  // Milliseconds for relay to switch (max 30ms) + margin of safety
 const int SET_PIN = 2;
 const int RESET_PIN = 3;
@@ -13,15 +11,20 @@ void setup() {
 
     pinMode(SET_PIN, OUTPUT);
     pinMode(RESET_PIN, OUTPUT);
+
+    Serial.println("Setup DONE");
 }
 
 // Trigger the relay to swap the batteries
 void swap() {
-    int swap_pin = (currentBattery == 0 || currentBattery == NULL) ? SET_PIN : RESET_PIN;
+    int swap_pin = currentBattery ? RESET_PIN : SET_PIN;
+
     digitalWrite(swap_pin, HIGH);
-    delay(SWAP_DURATION_MS)
+    delay(SWAP_DURATION_MS);
     digitalWrite(swap_pin, LOW);
     currentBattery = !currentBattery;
+
+    Serial.println("SWAP | Pin: " + String(swap_pin) + " | Run battery: " + String(currentBattery));
 }
 
 void loop() {
